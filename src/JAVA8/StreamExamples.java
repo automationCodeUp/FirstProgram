e30cdbeb995da0fc56463ade6dde7701ec082880
Raja C <rajarajan@Rajas-MacBook-Pro.local>
2024-06-07 14:36:37 +0530

    Add the modified program

--- src/JAVA8/StreamExamples.java
+++ src/JAVA8/StreamExamples.java
@@ -1,11 +1,17 @@
 package JAVA8;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamExamples {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(numbers.stream().reduce(0, Integer::sum));
         
-      System.out.println(numbers.stream().reduce(0, Integer::sum));
+        List<String> listofStrings = Arrays.asList("Raja","Sekar","Superb","shahrukh","Fantastic");
+        
+        List<String> listofFInalstr= listofStrings.stream().filter(name->name.startsWith("s")).map(s->s.toUpperCase()).collect(Collectors.toList());
+      
+        listofFInalstr.forEach(System.out::println);
     }
 }
